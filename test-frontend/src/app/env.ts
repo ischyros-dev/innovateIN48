d333bae098cbee68234b49ffc904eebfe481ce15
Jemar <loricajemar@gmail.com>
2021-09-12 21:07:06 +0800

    FE - commit operation

--- test-frontend/src/app/env.ts
+++ test-frontend/src/app/env.ts
@@ -1,1 +1,8 @@
 export const API_URL = 'http://localhost:5000'
+export const API_HOME = 'http://localhost:5000/home'
+export const API_CREATE = 'http://localhost:5000/create'    //post
+export const API_ENROL = 'http://localhost:5000/enrol'      //post
+export const API_INQUIRE = 'http://localhost:5000/inquire'  //get
+export const API_WITHDRAW = 'http://localhost:5000/withdraw' //post
+export const API_DEPOSIT = 'http://localhost:5000/deposit'  //post
+export const API_TRANSFER = 'http://localhost:5000/transfer' //post
